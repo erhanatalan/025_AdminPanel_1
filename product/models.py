829baa869cf489fdd540aded035fe546c60a7492
erhanatalan <erhanatalan2013@gmail.com>
2023-06-05 22:41:24 +0300

    reviews

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from ckeditor.fields import RichTextField
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.RichTextField(blank=True, null=True)
+    description = RichTextField(blank=True, null=True)
     is_in_stock = models.BooleanField(default=True)
     slug = models.SlugField(null=True, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
@@ -15,3 +15,16 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    review = models.TextField()
+    is_released = models.BooleanField(default=True)
+    created_date = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
+
+    def __str__(self):
+        return f"{self.product.name} - {self.review}" 
